48ee150be1b1bacb653940ad53d91d08c84eddb1
MeaTLoTioN <ml@erb.pw>
2019-08-28 13:44:28 +0100

    remove debug info from compile script

--- compile_nodelist.sh
+++ compile_nodelist.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 absfile=$(ls -rt outfile/* | tail -1)
-echo $absfile
-
 file=$(echo $(basename $absfile))
-echo $file
-
 ext=$(echo $file | awk -F. '{ print $2 }') 
-echo $ext
-
 newext="z${ext:1:2}" 
-echo $newext
-
 zip -j9 zip/tqwnet.$newext $absfile
 
