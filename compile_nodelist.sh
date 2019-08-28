92a3e0930f4afbb2905188ea277cb8daf6d3564b
MeaTLoTioN <ml@erb.pw>
2019-08-28 20:01:56 +0100

    modify userflags to include ZT for TQW for testing

--- compile_nodelist.sh
+++ compile_nodelist.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+ORIGDIR=$PWD
+WORKDIR="/root/git/tqwnet_nodelist"
+
+cd $WORKDIR
+
+makenl -d nodelist.txt
+
 absfile=$(ls -rt outfile/* | tail -1)
 file=$(echo $(basename $absfile))
 ext=$(echo $file | awk -F. '{ print $2 }') 
 newext="z${ext:1:2}" 
+
 zip -j9 zip/tqwnet.$newext $absfile
 
+cd $ORIGDIR
