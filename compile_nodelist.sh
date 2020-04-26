bfd5004535122fc0ecd10dff8b058ac9913a2f47
MeaTLoTioN <ml@erb.pw>
2020-04-26 19:35:34 +0100

    add script to update infopack and make a new zip

--- compile_nodelist.sh
+++ compile_nodelist.sh
@@ -2,7 +2,11 @@
 
 ORIGDIR=$PWD
 IWORKDIR="/root/git/tqwnet_nodelist"
+IPACKDIR="/root/git/tqwnet_infopack"
 WORKDIR="${1:-IWORKDIR}"
+PACKDIR="${2:-IPACKDIR}"
+
+COMMIT="$(date "+%Y-%m-%d %H:%M:%S")"
 
 cd $WORKDIR
 git pull
@@ -17,7 +21,18 @@ newext="z${ext:1:2}"
 zip -j9 zip/tqwnet.$newext $absfile
 
 git add . -A
-git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
+git commit -m "$COMMIT"
+git push
+
+cd $PACKDIR
+
+rm tqwnet.z*
+rm tqwinfo.zip
+cp $WORKDIR/zip/tqwnet.$newext .
+zip -9 tqwinfo.zip *
+
+git add . -A
+git commit -m "$COMMIT"
 git push
 
 cd $ORIGDIR
