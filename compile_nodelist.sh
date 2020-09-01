12750f4be2ee2b49fbb4db4e0ed621abd77cf8fc
christiansacks <christian@sacks.org.uk>
2020-09-01 13:03:05 +0100

    changes

--- compile_nodelist.sh
+++ compile_nodelist.sh
@@ -4,5 +4,6 @@ absfile=$(ls -rt outfile/* | tail -1)
 file=$(echo $(basename $absfile))
 ext=$(echo $file | awk -F. '{ print $2 }') 
 newext="z${ext:1:2}" 
-zip -j9 zip/tqwnet.$newext $absfile
+newfile="tqwnet.$newext"
+zip -j9 zip/$newfile $absfile
 
